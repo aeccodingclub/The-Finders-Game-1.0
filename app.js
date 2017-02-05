4b1ecffb7ff5995a6907ac3395e15cb84d259541
Prantick <krsnaprantick@gmail.com>
2017-02-05 08:24:11 +0530

    made and deleted env variables

--- app.js
+++ app.js
@@ -18,12 +18,8 @@ var indexRoutes		= require("./routes/index");
 var commentRoutes	= require("./routes/comment");
 var feedbackRoutes	= require("./routes/feedback");
 
-
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASEURL);
-// mongoose.connect("mongodb://Equinox:Equinox7@ds139869.mlab.com:39869/thefindersgame");
-
-
 
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
